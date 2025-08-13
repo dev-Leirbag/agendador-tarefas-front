9431e7aa395fbca7b9c681330456f70628b851b7
dev-LierBag <gabrielaf2186@gmail.com>
2025-08-12 23:55:57 -0300

    create register component interface

---
+++ src/app/pages/register/register.ts
@@ -0,0 +1,21 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-register',
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class Register {}
